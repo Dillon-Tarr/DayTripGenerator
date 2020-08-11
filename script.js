a9552c0e2d0763de96314ee5cb2e3c9029d344f9
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-08-10 23:48:14 -0400

    Created a blankStyleSheet, added some basic text to the web page, began working on the arrays for the program, and came up with a potential format for the final message that will be displayed in the console

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 "use strict";
 
-runProgram();
+//Arrays from which items will be randomly selected:
+let destinations = ["the Covid-19 wasteland AKA outside (don't forget your mask).", 'the backyard.', 
+'the room downstairs you still call the workout room even though no one has worked out in it since some time in February.', 
+"Grand Cayman -- in your head, that is; because you're not actually going to the Cayman Islands during this Covid mess, silly."];
+let restaurants = [];
+let modesOfTransportation = ['army crawls, and only army crawls.'];
+//When constructing the forms of entertainment, make them gerund phrases, to match the format of the final message.
+let formsOfEntertainment = [];
+
+function randomlySelectFromArray(array){
+
+}
+
+
+function displayFinalMessage(){
+    //console.log(`You'll be spending the day in ${destination}
+    //Under normal circumstances you might eat at a restaurant or two. Using your delivery service of choice, you'll be ordering from ${restaurant}
+    //You'll be getting around using ${modeOfTransportation}
+    //For entertainment, you will be ${formOfEntertainment}
+    //
+    //Thank you for choosing my little script to help you plan your trip. Enjoy!`)
+}
+
 
 function runProgram(){ //main function
 
@@ -8,3 +30,4 @@ function runProgram(){ //main function
 
 
 
+runProgram();
