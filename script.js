0c77f1fb120d2d9f4f45cd8254b800a6693c030f
Dillon Tarr <56458494+Dillon-Tarr@users.noreply.github.com>
2020-08-11 18:22:17 -0400

    Added elements to the arrays

--- script.js
+++ script.js
@@ -4,10 +4,11 @@
 let destinations = ["the Covid-19 wasteland AKA outside (don't forget your mask).", 'the backyard.', 
 'the room downstairs you still call the workout room even though no one has worked out in it since some time in February.', 
 "Grand Cayman -- in your head, that is; because you're not actually going to the Cayman Islands during this Covid mess, silly."];
-let restaurants = [];
-let modesOfTransportation = ['army crawls, and only army crawls.'];
-//When constructing the forms of entertainment, make them gerund phrases, to match the format of the final message.
-let formsOfEntertainment = [];
+let restaurants = ['Panera.', 'Chipotle.', 'Taco Bell.', 'Pizza Hut.', "Momma's home cookin', which is better than any restaurant."];
+let modesOfTransportation = ['army crawls, and only army crawls.', 'hand-walking, mostly.', 
+'those two legs of yours.', 'a skateboard.'];
+let formsOfEntertainment = ['reading a book or two of your choice.', 'binge watching Avatar: The Last Airbender.', 'surfing... the web.',
+'pacing aimlessly while calling all the people you should have checked up on during quarantine.'];
 
 function randomlySelectFromArray(array){
 
